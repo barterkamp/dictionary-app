f9465ae0cc95b5ed8a7638068346ca2a4ac36b6f
Bart Erkamp <bart.erkamp@maxserv.com>
2023-02-03 15:57:30 +0100

    add header with functional font-family and darkmode toggle

--- src/script.js
+++ src/script.js
@@ -2,4 +2,12 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
+Alpine.store('globals', {
+    selectFont : 'inter',
+    darkMode: false,
+    toggle() {
+        this.darkMode = !this.darkMode
+    },
+})
+
 Alpine.start()
