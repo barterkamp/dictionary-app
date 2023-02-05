371c17498267ffd9a6e7308f7d3a979943280179
Bart Erkamp <bart.erkamp@maxserv.com>
2023-02-05 15:33:01 +0100

    install axios & add first fetch results from dictionary API

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
@@ -8,6 +9,16 @@ Alpine.store('globals', {
     toggle() {
         this.darkMode = !this.darkMode
     },
+  word:'',
+  words: [],
+  getWord() {
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
+      .then((result) => {
+        this.words = result.data
+        console.log(this.words);
+      })
+      .catch((err) => console.log(err));
+  },
 })
 
 Alpine.start()
