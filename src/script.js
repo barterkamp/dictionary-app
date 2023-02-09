a728bafc42f1dbe2d516a111b3604c3bf0e61bb5
Bart Erkamp <bart.erkamp@maxserv.com>
2023-02-09 10:04:32 +0100

    add error status when a search is done without input

--- src/script.js
+++ src/script.js
@@ -4,21 +4,29 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.store('globals', {
-    selectFont : 'inter',
-    darkMode: false,
-    toggle() {
-        this.darkMode = !this.darkMode
-    },
+  selectFont : 'inter',
+  darkMode: false,
+  setError: false,
+  toggleDarkmode() {
+    this.darkMode = !this.darkMode
+  },
+  toggleError() {
+    this.setError = !this.setError
+  },
   word:'',
   words: [],
   getWord() {
+    if(this.word === '') {
+      this.setError = true;
+    } else {
     axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
       .then((result) => {
         this.words = result.data
+        this.setError = false;
         console.log(this.words);
       })
       .catch((err) => console.log(err));
-  },
+  }},
 })
 
 Alpine.start()
