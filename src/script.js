59ff11f937fa2309fe987fa535cafbcb4a899c9c
Bart Erkamp <bart.erkamp@maxserv.com>
2023-02-10 13:15:47 +0100

    add filter to find URL of audiofile / add functionality to play audio file

--- src/script.js
+++ src/script.js
@@ -1,42 +1,56 @@
-import axios from 'axios'
-import Alpine from 'alpinejs'
+import axios from "axios";
+import Alpine from "alpinejs";
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
-Alpine.store('globals', {
-  selectFont : 'inter',
+Alpine.store("globals", {
+  selectFont: "inter",
   darkMode: false,
   setError: false,
   toggleDarkmode() {
-    this.darkMode = !this.darkMode
+    this.darkMode = !this.darkMode;
   },
-  word:'',
+  word: "",
   words: [],
-  audio: [],
+  audioFile: "",
+
+  playAudio() {
+    let audio = new Audio(`${this.audioFile}`);
+    audio.play();
+  },
 
   errorMessage: {},
   showErrorMessage: false,
 
   getWord() {
-    if(this.word === '') {
+    if (this.word === "") {
       this.setError = true;
       this.showErrorMessage = false;
     } else {
-    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
-      .then((result) => {
-        this.words = result.data
-        this.showErrorMessage = false;
-        this.setError = false;
-      })
-      .catch((err) => {
-        this.errorMessage = err.response.data;
-        console.log(this.errorMessage);
-        this.showErrorMessage = true;
-        this.words = [];
-      });
-  }},
-})
+      axios
+        .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
+        .then((result) => {
+          this.words = result.data;
 
-Alpine.start()
+          // find a single audio file in the phonetics array
+          let audioArray = this.words.map((word) =>
+            word.phonetics
+              .filter((obj) => obj.hasOwnProperty("audio"))
+              .find((obj) => obj.audio)
+          );
+          this.audioFile = audioArray[0]["audio"];
 
+          this.showErrorMessage = false;
+          this.setError = false;
+        })
+        .catch((err) => {
+          this.errorMessage = err.response.data;
+          console.log(this.errorMessage);
+          this.showErrorMessage = true;
+          this.words = [];
+        });
+    }
+  },
+});
 
+Alpine.start();
