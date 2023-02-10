53b73aa44dcb6ba78989197c18227ed8aea55a62
Bart Erkamp <bart.erkamp@maxserv.com>
2023-02-10 08:19:19 +0100

    add error status when a user enters a mispelled or nonsensical word

--- src/script.js
+++ src/script.js
@@ -7,25 +7,43 @@ Alpine.store('globals', {
   selectFont : 'inter',
   darkMode: false,
   setError: false,
+
+
   toggleDarkmode() {
     this.darkMode = !this.darkMode
   },
-  toggleError() {
-    this.setError = !this.setError
-  },
+  // toggleError() {
+  //   this.setError = !this.setError
+  // },
   word:'',
   words: [],
+
+  errorMessage: {},
+  showErrorMessage: false,
+
+
   getWord() {
+
     if(this.word === '') {
       this.setError = true;
+      this.showErrorMessage = false;
     } else {
+
     axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
       .then((result) => {
         this.words = result.data
+        this.showErrorMessage = false;
         this.setError = false;
-        console.log(this.words);
+
+
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        this.errorMessage = err.response.data;
+        console.log(this.errorMessage);
+        this.showErrorMessage = true;
+        this.words = [];
+
+      });
   }},
 })
 
