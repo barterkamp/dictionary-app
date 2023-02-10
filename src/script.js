5a728240512cc16a1df632edbbd4cb3277bc1fd2
Bart Erkamp <bart.erkamp@maxserv.com>
2023-02-10 10:10:04 +0100

    add play button and order source links in a list

--- src/script.js
+++ src/script.js
@@ -7,42 +7,32 @@ Alpine.store('globals', {
   selectFont : 'inter',
   darkMode: false,
   setError: false,
-
-
   toggleDarkmode() {
     this.darkMode = !this.darkMode
   },
-  // toggleError() {
-  //   this.setError = !this.setError
-  // },
   word:'',
   words: [],
+  audio: [],
 
   errorMessage: {},
   showErrorMessage: false,
 
-
   getWord() {
-
     if(this.word === '') {
       this.setError = true;
       this.showErrorMessage = false;
     } else {
-
     axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
       .then((result) => {
         this.words = result.data
         this.showErrorMessage = false;
         this.setError = false;
-
-
       })
       .catch((err) => {
         this.errorMessage = err.response.data;
         console.log(this.errorMessage);
         this.showErrorMessage = true;
         this.words = [];
-
       });
   }},
 })
